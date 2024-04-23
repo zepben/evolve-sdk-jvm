dcf362c91c5ad3164ffceb48f0f3d255c002a159
Marcus Koh <marcus.koh@zepben.com>
2024-04-23 15:57:25 +1000

    Add metrics schema test
    Signed-off-by: Marcus Koh <marcus.koh@zepben.com>

--- src/test/kotlin/com/zepben/evolve/database/sqlite/metrics/MetricsWriterTest.kt
+++ src/test/kotlin/com/zepben/evolve/database/sqlite/metrics/MetricsWriterTest.kt
@@ -1,7 +1,6 @@
 package com.zepben.evolve.database.sqlite.metrics
 
-import com.zepben.evolve.metrics.IngestionJob
-import com.zepben.evolve.metrics.IngestionMetadata
+import com.zepben.evolve.metrics.*
 import com.zepben.testutils.junit.SystemLogExtension
 import io.mockk.every
 import io.mockk.mockk
@@ -18,17 +17,26 @@ internal class MetricsWriterTest {
     var systemErr: SystemLogExtension = SystemLogExtension.SYSTEM_ERR.captureLog().muteOnSuccess()
 
     private val metadata = IngestionMetadata(Instant.EPOCH, "N/A", "test", "0.0.0")
-    private val metrics = IngestionJob(UUID.randomUUID(), metadata)
-    private val metricsEntryWriter = mockk<MetricsEntryWriter> { every { save(any<IngestionMetadata>()) } returns true }
-    private val metricsWriter = MetricsWriter(metrics, mockk(), metricsEntryWriter)
+    private val job = IngestionJob(UUID.randomUUID(), metadata).apply {
+        sources["abc"]
+        networkMetrics[TotalNetworkContainer]
+    }
+    private val metricsEntryWriter = mockk<MetricsEntryWriter> {
+        every { save(any<IngestionMetadata>()) } returns true
+        every { saveSource(any<JobSource>()) } returns true
+        every { saveMetric(any<NetworkMetric>()) } returns true
+    }
+    private val metricsWriter = MetricsWriter(job, mockk(), metricsEntryWriter)
 
     @Test
     internal fun `passes objects through to the metrics entry writer`() {
-        // NOTE: the save method will fail due to the relaxed mock returning false for all save operations,
-        //       but a save should still be attempted on every object
         metricsWriter.save()
 
-        verify(exactly = 1) { metricsEntryWriter.save(metadata) }
+        verify(exactly = 1) {
+            metricsEntryWriter.save(metadata)
+            metricsEntryWriter.saveSource(job.sources.entries.first())
+            metricsEntryWriter.saveMetric(job.networkMetrics.entries.first())
+        }
     }
 
 }
